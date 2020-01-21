2f304670ff930de5f6251a7480f84bd2668b5a82
Christopher Spraggett <cspraggett@gmail.com>
2020-01-21 09:05:30 -0500

    Added script to start app with nodemon in package.json

--- express_server.js
+++ express_server.js
@@ -50,7 +50,6 @@ app.post("/urls", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
   res.redirect(longURL);
 });
 
